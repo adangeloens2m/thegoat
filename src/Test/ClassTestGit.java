617d3b40e28d7b6b44535aa790cc414860631f0e
Arnaud <Arnaud@192.168.0.17>
2020-03-19 16:39:05 +0100

    Test Github

--- src/Test/ClassTestGit.java
+++ src/Test/ClassTestGit.java
@@ -17,6 +17,7 @@ public class ClassTestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("AbC");
+        System.out.println("Ca va marcheeeeeeeer");
     }
     
 }
