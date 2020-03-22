b10dc680cf59e0b5e7fca01ba73384797d4987f9
Arnaud <Arnaud@192.168.0.17>
2020-03-22 17:49:39 +0100

    Test Github

--- src/TheGoatTuto/TilesTuto.java
+++ src/TheGoatTuto/TilesTuto.java
@@ -15,85 +15,55 @@ import javax.imageio.ImageIO;
 
 /**
  *
- * @author Arnaud
+ * @author Arnaud and Maxime
  */
-
-
 public class TilesTuto {
 
-    public int longueurChamp = 30;
-    public int largeurChamp = 15;
-
-    private int TILE_WIDTH = 32;
-    private int TILE_HEIGHT = 32;
+    private int TILE_WIDTH = 33;
+    private int TILE_HEIGHT = 33;
 
     private int TILES_PER_ROW = 20;
     private int TILES_PER_COL = 20;
 
-    private int[][] map;
-    private BufferedImage tileSheet;
+    private int[][] mat;
+    private BufferedImage tileSet;
 
-    public TilesTuto() {
-//        map = new int[existingMap.length][existingMap[0].length];
-//        for (int y = 0; y < map.length; y++) {
-//            for (int x = 0; x < map[y].length; x++) {
-//                map[y][x] = existingMap[y][x];
-//            }
-//        }
-        int[][] mat = new int[largeurChamp][longueurChamp];
+    public TilesTuto(int largeurChamp, int longueurChamp) {
+        mat = new int[largeurChamp][longueurChamp];
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
-                if (i == 1) {
+                if (i == 0 || i == largeurChamp - 1) {
                     mat[i][j] = 184;
                 } else {
                     mat[i][j] = 21;
                 }
             }
         }
+
         try {
-            tileSheet = ImageIO.read(new File("tileSet.png"));
+            tileSet = ImageIO.read(new File("tileSet.png"));
         } catch (IOException ex) {
             Logger.getLogger(Tiles.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     public void DrawLayer(Graphics g) {
-//        for (int y = 0; y < map.length; y++) {
-//            for (int x = 0; x < map[y].length; x++) {
-//                int index = map[y][x];
-//                int yOffset = 0;
-//
-//                if (index > (tileSheet.getWidth() / TILE_WIDTH) - 1) {
-//                    yOffset++;
-//                    index = index - (tileSheet.getWidth() / TILE_WIDTH);
-//                }
-//
-//                g.drawImage(tileSheet,
-//                        x * TILE_WIDTH,
-//                        y * TILE_HEIGHT,
-//                        (x * TILE_WIDTH) +  TILE_WIDTH,
-//                        (y * TILE_HEIGHT) + TILE_HEIGHT,
-//                        index * TILE_WIDTH,
-//                        yOffset * TILE_HEIGHT,
-//                        (index * TILE_WIDTH) + TILE_WIDTH,
-//                        (yOffset * TILE_HEIGHT) + TILE_HEIGHT,
-//                        null);
-//            }
-//        }
-        int xFenetreG = 0;
-        int yFenetreG = 0;
-        for (int i = 0; i < largeurChamp; i++) {
-            for (int j = 0; j < longueurChamp; j++) {
-                xFenetreG = (j) * TILE_WIDTH;
-                yFenetreG = (i) * TILE_HEIGHT;
-                int tileNumber = map[i][j];
-                int tileX = (tileNumber % TILES_PER_ROW) * TILE_WIDTH + tileNumber % TILES_PER_ROW + 1;
-                int tileY = (tileNumber / TILES_PER_ROW) * TILE_HEIGHT + tileNumber / TILES_PER_ROW + 1;
-                BufferedImage tile = tileSheet.getSubimage(tileX, tileY, TILE_WIDTH, TILE_HEIGHT);
+        for (int j = 0; j < mat.length; j++) {
+            for (int i = 0; i < mat[j].length; i++) {
+                int colonneTileSet = mat[j][i] % TILES_PER_ROW;
+                int ligneTileSet = mat[j][i] / TILES_PER_ROW;
 
-                g.drawImage(tile, xFenetreG, yFenetreG, null);
+                g.drawImage(tileSet,
+                        i * TILE_WIDTH,
+                        j * TILE_HEIGHT,
+                        (i * TILE_WIDTH) + TILE_WIDTH,
+                        (j * TILE_HEIGHT) + TILE_HEIGHT,
+                        colonneTileSet * TILE_WIDTH + 1,
+                        ligneTileSet * TILE_HEIGHT + 1,
+                        (colonneTileSet * TILE_WIDTH) + TILE_WIDTH,
+                        (ligneTileSet * TILE_HEIGHT) + TILE_HEIGHT,
+                        null);
             }
-
         }
     }
 }
