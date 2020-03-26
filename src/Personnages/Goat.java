a17aa85e4326e4806ed8e2f6459093e38abad3bc
Arnaud <Arnaud@192.168.0.17>
2020-03-26 19:28:28 +0100

    Tile Mapping

--- src/Personnages/Goat.java
+++ src/Personnages/Goat.java
@@ -5,6 +5,7 @@
  */
 package Personnages;
 
+import TheGoatTuto.Main;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 
@@ -12,29 +13,57 @@ import javax.swing.ImageIcon;
  *
  * @author maxime.bulabois
  */
-public class Goat extends Personnage{
-    
+public class Goat extends Personnage {
+
     private Image imageGoat;
     private ImageIcon iconGoat;
-    
+
     private String pseudo;
-    
+
     private int nbVie;
-    
+
     private int skin;
-    
+
+    private int dx;
+    private int dy;
+
     //CONSTRUCTEUR
-    public Goat(int x, int y, String pseudo){
-        super(x, y, 50, 50);
+    public Goat(int x, int y, String pseudo, String skin) {
+        super(x, y, 80, 80);
         this.pseudo = pseudo;
-        this.skin = skin;
-        this.iconGoat = new ImageIcon(getClass().getResource("/images/goat.png"));
-        this.imageGoat = this.iconGoat.getImage();
+        this.iconGoat = new ImageIcon(getClass().getResource("/images/" + skin + ".png"));
+        this.imageGoat = this.iconGoat.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH);
     }
-    
+
     //GETTERS
-    public Image getImageGoat(){
+    public Image getImageGoat() {
         return imageGoat;
     }
     
+    //Setters
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
+    
+    
+    //Methodes
+        public void deplacement() {
+        if (dx == 1) {
+            x = Math.min(x + dx, Main.scene.getWidth() - imageGoat.getWidth(null));
+        }
+        if (dx == -1) {
+            x = Math.max(x + dx, 0);
+        }
+        if (dy == 1) {
+            y = Math.min(y + dy, Main.scene.getHeight() - imageGoat.getHeight(null));
+        }
+        if (dy == -1) {
+            y = Math.max(y + dy, 0);
+        }
+    }
 }
