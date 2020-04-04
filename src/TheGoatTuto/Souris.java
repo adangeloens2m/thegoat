3ca1b8727b9dbe0025f76c3d47ca481c46f70b41
Arnaud <Arnaud@192.168.0.17>
2020-04-04 20:09:52 +0200

    Multi-joueurs

--- src/TheGoatTuto/Souris.java
+++ src/TheGoatTuto/Souris.java
@@ -10,9 +10,7 @@ import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import outil.OutilsJDBC;
 
 /**
  *
