85449c8be4f01d2bf4adaefcc863bd89e6d9560a
Arnaud <Arnaud@192.168.0.17>
2020-04-04 18:14:18 +0200

    Multi-joueurs

--- src/TheGoatMultiJoueurs/Clavier.java
+++ src/TheGoatMultiJoueurs/Clavier.java
@@ -22,16 +22,9 @@ public class Clavier extends KeyAdapter {
         if (Main.scene.getPersonnage() == "goat") {
 
             try {
-                //Connection connexion = DriverManager.getConnection("jdbc:mysql://nemrod.ens2m.fr:3306/20192020_s2_vs2_tp1_goat?serverTimezone=UTC", "goat", "9FdqUt5uXibSkOF8");
                 PreparedStatement requete = ConnexionBDD.getInstance().prepareStatement("UPDATE goat, piege "
-                        + "SET goat.x = goat.x + ?, goat.x = goat.x - ?, goat.y = goat.y - ?, goat.y = goat.y + ?, piege.nbVie = ? WHERE pseudo = ?");
-                
-                PreparedStatement requete1 = ConnexionBDD.getInstance().prepareStatement("DELETE * FROM piege");
-                
-                if (e.getKeyCode() == KeyEvent.VK_R){
-                    requete.setInt(5, 5);
-                    requete1.executeUpdate();
-                }
+                        + "SET goat.x = goat.x + ?, goat.x = goat.x - ?, goat.y = goat.y - ?, goat.y = goat.y + ? "
+                        + "WHERE pseudo = ?");
                 if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
                     requete.setInt(1, 1);
                 } else {
@@ -52,13 +45,25 @@ public class Clavier extends KeyAdapter {
                 } else {
                     requete.setInt(4, 0);
                 }
-                
-                requete.setString(6, Main.scene.getPseudo());
+                requete.setString(5, Main.scene.getPseudo());
+
                 requete.executeUpdate();
 
                 requete.close();
-                requete1.close();
-                //connexion.close();
+
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        if (e.getKeyCode() == KeyEvent.VK_R) {
+
+            try {
+
+                PreparedStatement requete = ConnexionBDD.getInstance().prepareStatement("DELETE FROM piege");
+                
+                requete.executeUpdate();
+
+                requete.close();
 
             } catch (SQLException ex) {
                 ex.printStackTrace();
